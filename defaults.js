2a9e3b0f977582335809e360795f31f8dcae26e2
prateekrajgautam <prateekrajgautam@gmail.com>
2022-08-04 01:34:11 +0530

    use single script file

--- defaults.js
+++ defaults.js
@@ -1,5 +1,5 @@
 //SiteDefaults
-const site = {}
+var site = {}
 site.title = "GeneratorJs"
 site.site_title = site.title
 site.subtitle = "A three function Javascript library for modular website frontend desing"
@@ -8,19 +8,13 @@ site.image = site.logo
 site.favicon = '/assets/img/favicon.ico'
 site.image = site.logo
 site.description = '"A three function Javascript library for modular website frontend desing. Few functions to generate frontend design without juggeling between HTML CSS and JS. Reduce repetetive typing in HTML.'
-site.url = 'https://generatorjs.github.io/'
+site.url = 'https://generatorjs.mgeek.in/'
 
-var s = site
 
-//social links JSON or Js dict
-const social = [
+var social = [
     { tag: 'a', id: 'fb', innerHTML: 'Facebook', href: "https://facebook.com/mGeek.in", target: "_blank", classList: 'sociallinks' },
     { tag: 'a', id: 'tg', innerHTML: 'Telegram', href: "https://t.me/mGeek", target: "_blank", classList: 'sociallinks' },
     { tag: 'a', id: 'git', innerHTML: 'Github', href: "https://github.com/GeneratorJs/generatorjs.cdn.git", target: "_blank", classList: 'sociallinks' },
     { tag: 'a', id: 'mail', innerHTML: 'Email', href: `mailto:care@mgeek.in?subject=Related to GeneratorJs&body=From : ${window.location.href}`, target: "_blank", classList: 'sociallinks' }
 ]
-export { s as site }
-export { social as social }
 
-window.site = site
-window.social = social

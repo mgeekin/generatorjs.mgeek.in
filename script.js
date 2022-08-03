b714e941d7ab4c9d2f99a968dbddfd1f318e4699
prateekrajgautam <prateekrajgautam@gmail.com>
2022-08-04 01:44:30 +0530

    single 36.1kb generatorjs file

--- script.js
+++ script.js
@@ -18,15 +18,24 @@ append(appmain, gen(section, "install", gen(h1, "", "Install"), 'section contain
 append(install, gen(div, "github", gen(h2, "", "Clone from Github")))
 append(github, gen(code, '', `git clone https://github.com/GeneratorJs/generatorjs.cdn.git`, 'code, small'))
 //html include script tag
-append(install, gen(div, "scriptTag", gen(h2, "", "Add script module tag in html")))
+append(install, gen(div, "scriptTag", gen(h2, "", "Add script tag in html")))
 
-append(scriptTag, gen(code, '', ` <script type="module">
-import { gen, append, load } from "https://generatorjs.github.io/generatorjs.cdn/dist/latest/generator.modular.js"
-//your frontend logic using generatorjs
-append(app,gen(h1,"mainheading","Hello world from GeneratorJs","stylingclass"))
-</script>`, 'code,small'))
+append(scriptTag, gen(code, '', `
+<script src="https://generatorjs.github.io/generatorjs.cdn/generator.js"></script>
+`, 'code,small'))
 
-append(scriptTag, gen(code, '', ` <script type="module" src="https://generatorjs.github.io/generatorjs.cdn/dist/latest/generator.modular.js"></script>`, 'code, small'))
+append(scriptTag, gen(code, '', `
+<script src="https://generatorjs.github.io/generatorjs.cdn/generator.js"></script>
+<script defer>
+    window.$ = GeneratorJs()
+    $.init()
+
+    //your app logic
+    append(app, "", "over") /* reset app */
+    append(app, gen(header, "appheader", 'Header content', 'header'));
+    append(app, gen(main, "appmain", 'Main content', 'appmain container'));
+    append(app, gen(footer, "appfooter", "Footer content", 'container'));
+</script>`, 'code, small'))
 
 append(scriptTag, gen(p, "", "By default it will try to load 'script.js from dir where index.html is located, so you can write code in script.js and place it with 'index.html'"))
 
